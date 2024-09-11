f174e58ef235b5c8288471c4f8d7d0a43871c6d9
arpadav <arpadav@gmail.com>
2024-09-11 14:04:46 -0400

    prog 0903

--- src/misb0903/ops.rs
+++ src/misb0903/ops.rs
@@ -7,30 +7,114 @@ use tinyklv::prelude::*;
 // static
 // --------------------------------------------------
 use std::sync::LazyLock;
+
 /// 2 byte-precision in range [0, 180]
 /// 
 /// Used for some angles
 /// 
 /// Units: Degrees (°)
-static IMAPB_0_180_2_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
+pub(crate) static IMAPB_0_180_2_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
     crate::misb1201::ImapB::new(0.0, 180.0, 2).unwrap()
 });
+
+/// 3 byte-precision in range [-19.2, 19.2]
+/// 
+/// Used for latitude / longitude offsets
+/// 
+/// Units: Degrees (°)
+pub(crate) static IMAPB_N19P2_19P2_3_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
+    crate::misb1201::ImapB::new(-19.2, 19.2, 3).unwrap()
+});
+
+/// 4 byte-precision in range [-90.0, 90.0]
+/// 
+/// Used for latitude
+/// 
+/// Units: Degrees (°)
+pub(crate) static IMAPB_N90_90_4_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
+    crate::misb1201::ImapB::new(-90.0, 90.0, 4).unwrap()
+});
+
+/// 4 byte-precision in range [-180.0, 180.0]
+/// 
+/// Used for longitude
+/// 
+/// Units: Degrees (°)
+pub(crate) static IMAPB_N180_180_4_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
+    crate::misb1201::ImapB::new(-180.0, 180.0, 4).unwrap()
+});
+
+/// 2 byte-precision in range [-900, 900]
+/// 
+/// Used for velocity / acceleration
+/// 
+/// Units: Meters per second (m/s) or Meters per second squared (m/s2)
+pub(crate) static IMAPB_N900_900_2_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
+    crate::misb1201::ImapB::new(-900.0, 900.0, 2).unwrap()
+});
+
+/// 2-byte precision in range [0, 650]
+/// 
+/// Used for standard-deviation of position
+/// 
+/// Units: Meters (m)
+pub(crate) static IMAPB_0_650_2_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
+    crate::misb1201::ImapB::new(0.0, 650.0, 2).unwrap()
+});
+
+/// 2 byte-precision in range [-1, 1]
+/// 
+/// Used for correlation-coefficients
+/// 
+/// Units: None
+pub(crate) static IMAPB_N1_1_2_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
+    crate::misb1201::ImapB::new(-1.0, 1.0, 2).unwrap()
+});
+
+/// 2 byte-precision in range [-900, 19_000]
+/// 
+/// Used for HAE (height above ellipsoid)
+/// 
+/// Units: Meters (m)
+pub(crate) static IMAPB_N900_19K_2_F64: LazyLock<crate::misb1201::ImapB<f64>> = LazyLock::new(|| {
+    crate::misb1201::ImapB::new(-900.0, 19_000.0, 2).unwrap()
+});
+
+/// General parser wrapper for [`crate::misb1201::ImapB`]
+pub fn imapb_parser<T: crate::misb1201::ImapFloat + 'static> (
+    imap: &'static LazyLock<crate::misb1201::ImapB<T>>,
+    len: usize,
+) -> impl Fn(&mut &[u8]) -> winnow::PResult<T> {
+    move |input: &mut &[u8]| {
+        let checkpoint = input.checkpoint();
+        let value = winnow::token::take(len).parse_next(input)?;
+        imap.from_imap(value).map_err(|e| tinyklv::err!().add_context(input, &checkpoint, e.into()))
+    }
+}
 
 /// See: [`crate::misb0903::Misb0903`]
 /// 
 /// * [`crate::misb0903::Misb0903::vmti_hfov`]
 /// * [`crate::misb0903::Misb0903::vmti_vfov`]
 pub fn to_hvfov(input: &mut &[u8]) -> winnow::PResult<f64> {
-    let checkpoint = input.checkpoint();
-    IMAPB_0_180_2_F64
-        .from_imap(input)
-        .map_err(|e| tinyklv::err!().add_context(input, &checkpoint, e.into()))
+    imapb_parser(&IMAPB_0_180_2_F64, 2).parse_next(input)
 }
 
-pub fn to_row_col(input: &mut &[u8], len: usize) -> winnow::PResult<(u32, u32)> {
-    let pixel_value = tinyklv::binary::dec::be_u32_lengthed(input, len)?;
-    let frame_width = 100.0;
-    let row = ((pixel_value as f32 / frame_width)).floor() as u32 + 1;
-    let col = ((pixel_value - (row - 1)) as f32 * frame_width) as u32;
-    Ok((row, col))
+/// See: [`crate::misb0903::Misb0903`]
+/// 
+/// * [`crate::misb0903::Misb0903Target::target_location_lat_offset`]
+/// * [`crate::misb0903::Misb0903Target::target_location_lon_offset`]
+/// * [`crate::misb0903::Misb0903Target::bbox_tl_lat_offset`]
+/// * [`crate::misb0903::Misb0903Target::bbox_tl_lon_offset`]
+/// * [`crate::misb0903::Misb0903Target::bbox_br_lat_offset`]
+/// * [`crate::misb0903::Misb0903Target::bbox_br_lon_offset`]
+pub fn to_ll_offset(input: &mut &[u8]) -> winnow::PResult<f64> {
+    imapb_parser(&IMAPB_N19P2_19P2_3_F64, 3).parse_next(input)
+}
+
+/// See: [`crate::misb0903::Misb0903`]
+/// 
+/// * [`crate::misb0903::Misb0903Target::target_hae`]
+pub fn to_hae(input: &mut &[u8]) -> winnow::PResult<f64> {
+    imapb_parser(&IMAPB_N900_19K_2_F64, 2).parse_next(input)
 }
